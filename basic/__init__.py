07f2e07500f7fe6a6c8d9996884eee847133b600
生井沢祥吾 <rawswamp460@gmail.com>
2025-10-10 11:23:43 +0900

    making demographic page in basic

--- basic/__init__.py
+++ basic/__init__.py
@@ -21,43 +21,66 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    # デモグラフィック情報
     q_gender = models.CharField(
         initial=None,
-        choices=['男性', '女性', '回答しない'],
-        verbose_name='あなたの性別を教えてください。',
+        choices=['男性', '女性', 'その他', '回答しない'],
+        verbose_name='<span style="font-size: 1.4rem; font-weight: 900;">1. あなたの性別を教えてください。</span>',
         widget=widgets.RadioSelect
     )
 
-    q_age = models.IntegerField(
+    q_age = models.CharField(
         initial=None,
-        verbose_name='あなたの年齢を教えてください。',
-        choices=range(0, 125)
+        choices=['19歳以下', '20-29歳', '30-39歳', '40-49歳', '50-59歳', '60-69歳', '70歳以上'],
+        verbose_name='<span style="font-size: 1.4rem; font-weight: 900;">2. あなたの年代を教えてください。</span>',
+        widget=widgets.RadioSelect
     )
 
     q_area = models.CharField(
         initial=None,
         choices=['北海道', '東北地方', '関東地方',
                 '中部地方', '近畿地方', '中国地方', 
-                '四国地方', '九州地方'], 
-        verbose_name='お住まいの地方を教えてください。'
+                '四国地方', '九州地方', '回答しない'], 
+        verbose_name='<span style="font-size: 1.4rem; font-weight: 900;">3. お住まいの地方を教えてください。</span>',
+        widget=widgets.RadioSelect
+    )
+
+    q_education = models.CharField(
+        initial=None,
+        choices=['中学校卒業', '高等学校卒業', '専門学校卒業', '短期大学卒業', 
+                '大学卒業', '大学院修了（修士）', '大学院修了（博士）', 'その他'],
+        verbose_name='<span style="font-size: 1.4rem; font-weight: 900;">4. あなたの最終学歴を教えてください。</span>',
+        widget=widgets.RadioSelect
     )
 
     q_device = models.CharField(
         initial=None,
         choices=['パソコン', 'タブレット', 'スマートフォン', 'それ以外'], 
-        verbose_name='回答端末を教えてください',
+        verbose_name='<span style="font-size: 1.4rem; font-weight: 900;">5. 現在お使いの回答端末を教えてください。</span>',
         widget=widgets.RadioSelect
     )
 
 
 # PAGES
-class Page1(Page):
-    form_model = 'player'
-    form_fields = ['q_gender', 'q_age', 'q_area', 'q_device']
-
-
-class Results(Page):
+class StartPage(Page):
+    """アンケート開始ページ"""
     pass
 
 
-page_sequence = [Page1, Results]
+class HonestyPage(Page):
+    """誠実性確認ページ"""
+    pass
+
+
+class DemographicPage(Page):
+    """デモグラフィック情報収集ページ"""
+    form_model = 'player'
+    form_fields = ['q_gender', 'q_age', 'q_area', 'q_education', 'q_device']
+
+
+class Results(Page):
+    """完了ページ"""
+    pass
+
+
+page_sequence = [StartPage, HonestyPage, DemographicPage, Results]
